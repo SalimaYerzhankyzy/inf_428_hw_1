aabe5dec70acc6c00295fdbc1eb4b77ceada5bd2
SalimaYerzhankyzy <104423184+SalimaYerzhankyzy@users.noreply.github.com>
2024-11-21 22:02:13 +0600

    Update Aggregated User Threat Score.py
    Updated version of task 2

--- Task 2/Aggregated User Threat Score.py
+++ Task 2/Aggregated User Threat Score.py
@@ -6,78 +6,69 @@ def generate_random_data(mean, variance, num_samples):
     return np.random.randint(max(mean - variance, 0), min(mean + variance + 1, 90), num_samples)
 
 def aggregated_threat_score(departments):
-    total_score = 0
-    total_importance = 0
+    department_scores = []
 
+    # Calculating threat score of each department and normalizing it based on department size(users amount)
     for department in departments:
         users = department['users']
-        importance = department['importance']
-        average_threat_score = np.mean(users)
+        mean_threat = np.mean(users)
+        variance_threat = np.var(users)
 
-        # Calculating average threat score based on importance
-        priority_threat_score = average_threat_score * importance
-        total_score += priority_threat_score
-        total_importance += importance
-
-    if total_importance == 0:
-        return 0
-
-    aggregated_score = total_score / total_importance
+        department_scores.append(mean_threat + variance_threat)
 
     # Normalization the aggregated score (range 0 - 90)
-    return min(max(0, aggregated_score), 90)
+    aggregated_score = np.mean(department_scores)
+    return min(max(0, int(aggregated_score)), 90)
 
 
 #Tests
 class Test(unittest.TestCase):
 
-    #If departments importance is zero. Should return a score as 0
-    def test_zero_importance(self):
+    # All departments has similar threat scores
+    def test_similar_threat_score(self):
         departments = [
-            {'users': generate_random_data(30, 10, 10), 'importance': 0},
-            {'users': generate_random_data(10, 10, 10), 'importance': 0}
+            {'users': generate_random_data(40, 5, 30)},
+            {'users': generate_random_data(37, 5, 30)},
+            {'users': generate_random_data(43, 5, 30)}
         ]
         score = aggregated_threat_score(departments)
-        self.assertEqual(score, 0)
+        print(f"Test Similar Threat Score: Computed Score = {score}")
+        self.assertTrue(30 <= score <= 50)
 
-    # All the same threat scores and importance
-    def test_same_input(self):
+    # One department has a high threat score(outlier)
+    def test_outlier_department(self):
         departments = [
-            {'users': generate_random_data(50, 10, 10), 'importance': 3},
-            {'users': generate_random_data(50, 10, 10), 'importance': 3},
-            {'users': generate_random_data(50, 10, 10), 'importance': 3}
+            {'users': generate_random_data(20, 10, 30)},
+            {'users': generate_random_data(10, 10, 30)},
+            {'users': generate_random_data(80, 10, 30)}
         ]
         score = aggregated_threat_score(departments)
-        self.assertTrue(0 <= score <= 90)
+        print(f"Test Outlier Department: Computed Score = {score}")
+        self.assertTrue(score > 50)
 
-    #If one departments threat score is high and others less
-    def test_threat_score(self):
+    # Users with really high scores inside one of the departments (other departments have a low score)
+    def test_outlier_users(self):
         departments = [
-            {'users': generate_random_data(90, 10, 10), 'importance': 5},
-            {'users': generate_random_data(10, 10, 10), 'importance': 1}
+            {'users': generate_random_data(20, 10, 30)},
+            {'users': generate_random_data(10, 10, 30)},
+            {'users': np.append(generate_random_data(15, 10, 25), [90, 90, 90, 90, 90])} # five users with high score
         ]
         score = aggregated_threat_score(departments)
-        self.assertTrue(0 <= score <= 90)
+        print(f"Test Outlier Users: Computed Score = {score}")
+        self.assertTrue(70 <= score <= 90)
 
-    #Everything at the high peak
-    def test_high_input(self):
+    # Departments with varying numbers of users
+    def test_varying_users_amount(self):
         departments = [
-            {'users': generate_random_data(90, 10, 10), 'importance': 4},
-            {'users': generate_random_data(80, 10, 10), 'importance': 5}
+            {'users': generate_random_data(80, 10, 10)},
+            {'users': generate_random_data(45, 10, 200)},
+            {'users': generate_random_data(50, 10, 20)}
         ]
         score = aggregated_threat_score(departments)
-        self.assertTrue(0 <= score <= 90)
-
-    #Different mixed input values
-    def test_mixed(self):
-        departments = [
-            {'users': generate_random_data(30, 10, 10), 'importance': 1},
-            {'users': generate_random_data(70, 10, 10), 'importance': 4}
-        ]
-        score = aggregated_threat_score(departments)
-        self.assertTrue(0 <= score <= 90)
+        print(f"Test Varying Users Amount: Computed Score = {score}")
+        self.assertTrue(score >= 70)
 
 
-# Running tests
+#Running tests
 if __name__ == '__main__':
     unittest.main()
